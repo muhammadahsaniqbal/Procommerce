f841aac1f71cfdb775b4fc70fa78f379653eb596
Ahsan Iqbal <muhammadahsan.iqbal@maqta.ae>
2021-01-23 03:53:35 +0400

    Configured tabs for Home & Profile screens

--- src/screens.js
+++ src/screens.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigation } from 'react-native-navigation';
 import Home from './screens/Home';
 import Landing from './screens/Landing';
+import Profile from './screens/Profile';
 
 // register all screens of the app (including internal ones)
 export default function registerScreens(store, Provider) {
@@ -17,4 +18,10 @@ export default function registerScreens(store, Provider) {
             <Home {...props} />
         </Provider>,
         () => Home);
+
+    Navigation.registerComponent(`Profile`, () => (props) =>
+        <Provider store={store}>
+            <Profile {...props} />
+        </Provider>,
+        () => Profile);
 }
--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -26,19 +26,83 @@ class Landing extends Component {
 
     moveToHomeScreen() {
         Navigation.push(this.props.componentId, {
-            component: {
-                name: 'Home',
-                options: {
-                    popGesture: false,
-                    topBar: {
-                        title: {
-                            text: 'Home'
-                        },
-                        backButton: {
-                            visible: false
+            bottomTabs: {
+                id: 'BOTTOM_TABS_LAYOUT',
+                children: [
+                    {
+                        stack: {
+                            id: 'HOME_TAB',
+                            children: [
+                                {
+                                    component: {
+                                        id: 'HOME_SCREEN',
+                                        name: 'Home',
+                                        options: {
+                                            popGesture: false,
+                                            topBar: {
+                                                title: {
+                                                    text: 'Home'
+                                                },
+                                                backButton: {
+                                                    visible: false
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                            options: {
+                                bottomTab: {
+                                    icon: require('../assets/tabs/home.png'),
+                                    iconInsets: { top: 20, left: 0, bottom: -20, right: 0 },
+                                    iconColor: '#acacac',
+                                    selectedIconColor: '#000000'
+                                },
+                            }
+                        }
+                    },
+                    {
+                        stack: {
+                            id: 'PROFILE_TAB',
+                            children: [
+                                {
+                                    component: {
+                                        id: 'PROFILE_SCREEN',
+                                        name: 'Profile',
+                                        options: {
+                                            popGesture: false,
+                                            topBar: {
+                                                title: {
+                                                    text: 'Profile'
+                                                },
+                                                backButton: {
+                                                    visible: false
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                            options: {
+                                bottomTab: {
+                                    icon: require('../assets/tabs/profile.png'),
+                                    iconInsets: { top: 20, left: 0, bottom: -20, right: 0 },
+                                    iconColor: '#acacac',
+                                    selectedIconColor: '#000000'
+                                },
+                            }
                         }
                     }
-                }
+                ],
+                options: {
+                    topBar: {
+                        visible: false
+                    },
+                    bottomTabs: {
+                        backgroundColor: '#dddddd',
+                        animate: true
+                    },
+                },
             }
         })
     }
